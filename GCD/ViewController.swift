2b346c220ce5c46bcdf7d14618c45efe3bb2c2a5
GrandreChina <Grandre@zhangyingdeMacBook-Air.local>
2016-07-25 20:09:33 +0800

    GCD

--- GCD/ViewController.swift
+++ GCD/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //    GCD
+    func delay(second:Double,block:()->()){
+        let Second = second * Double(NSEC_PER_SEC)  //0指等待0秒播放动画
+        let dtime = dispatch_time(DISPATCH_TIME_NOW, Int64(Second))
+        dispatch_after(dtime, dispatch_get_main_queue(), block)
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
